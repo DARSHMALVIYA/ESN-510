655672c02daff54a6187498c4bef5002185b04dc
Darshan Malviya <90540117+DARSHMALVIYA@users.noreply.github.com>
2023-08-28 12:43:24 +0530

    Add files via upload

---
+++ loop_monday.m
@@ -0,0 +1,15 @@
+for n = 1:5
+x=n
+    if x>3
+        disp('these number are greater than 3')
+    else
+        disp('these number are lessser than 3')
+    end
+end
+%================================
+Y=0;
+for n = 1:3;
+  x=2*n^2+n+1;
+  Y=Y+x;
+end
+ disp(Y)
